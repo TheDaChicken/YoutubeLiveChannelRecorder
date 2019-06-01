046bac4db88b8890be1366b903e8eef6a0fab482
TheDaChicken <steve.rock.pet@gmail.com>
2019-05-31 18:01:49 -0700

    Stupid big if statement but used to stop repeated getting youtube variables.

--- SERVER/PYTHON-FILES/Code/youtube/__init__.py
+++ SERVER/PYTHON-FILES/Code/youtube/__init__.py
@@ -26,33 +26,35 @@ def set_global_youtube_variables(html_code=None):
     global utf_offset
     global client_name
 
-    youtube_player_config = get_yt_player_config(html_code)
-    youtube_initial_data = get_yt_initial_data(html_code)
-    e_catcher = getServiceSettings(try_get(youtube_initial_data, lambda x: x['responseContext'][
-        'serviceTrackingParams'], list), "ECATCHER")
+    if not account_playback_token and not page_build_label and not page_cl and not variants_checksum and not \
+            client_version and not utf_offset and not client_name:
+        youtube_player_config = get_yt_player_config(html_code)
+        youtube_initial_data = get_yt_initial_data(html_code)
+        e_catcher = getServiceSettings(try_get(youtube_initial_data, lambda x: x['responseContext'][
+            'serviceTrackingParams'], list), "ECATCHER")
 
-    if not youtube_player_config:
-        warning("Unable to find Youtube Player Config. Cannot find all Youtube Variables.")
-    else:
-        verbose("Getting Playback Token. [GLOBAL YOUTUBE]")
-        account_playback_token = try_get(youtube_player_config, lambda x: x['args']['account_playback_token'][:-1], str)
-        if account_playback_token is None:
-            warning("Something happened when finding the account "
-                    "playback Token.")
-            return None
+        if not youtube_player_config:
+            warning("Unable to find Youtube Player Config. Cannot find all Youtube Variables.")
+        else:
+            verbose("Getting Playback Token. [GLOBAL YOUTUBE]")
+            account_playback_token = try_get(youtube_player_config, lambda x: x['args']['account_playback_token'][:-1], str)
+            if account_playback_token is None:
+                warning("Something happened when finding the account "
+                        "playback Token.")
+                return None
 
-    if not youtube_initial_data:
-        warning("Unable to find Youtube Initial Data. Cannot find all Youtube Variables.")
-    elif not e_catcher:
-        warning("Unable to find ECATCHER service data in Youtube Initial Data. Cannot find Youtube Variables.")
-    else:
-        params = try_get(e_catcher, lambda x: x['params'], list)
-        page_build_label = getSettingsValue(params, 'innertube.build.label', name="Page Build Label")
-        page_cl = getSettingsValue(params, 'innertube.build.changelist', name="Page CL")
-        variants_checksum = getSettingsValue(params, 'innertube.build.variants.checksum', name="Variants Checksum")
-        client_version = getSettingsValue(params, 'client.version', name="Client Version")
-        utf_offset = get_utc_offset()
-        client_name = getSettingsValue(params, 'client.name', name="Client Name")
+        if not youtube_initial_data:
+            warning("Unable to find Youtube Initial Data. Cannot find all Youtube Variables.")
+        elif not e_catcher:
+            warning("Unable to find ECATCHER service data in Youtube Initial Data. Cannot find Youtube Variables.")
+        else:
+            params = try_get(e_catcher, lambda x: x['params'], list)
+            page_build_label = getSettingsValue(params, 'innertube.build.label', name="Page Build Label")
+            page_cl = getSettingsValue(params, 'innertube.build.changelist', name="Page CL")
+            variants_checksum = getSettingsValue(params, 'innertube.build.variants.checksum', name="Variants Checksum")
+            client_version = getSettingsValue(params, 'client.version', name="Client Version")
+            utf_offset = get_utc_offset()
+            client_name = getSettingsValue(params, 'client.name', name="Client Name")
 
 
 def getServiceSettings(serviceTrackingParamsList, service_nameLook):
