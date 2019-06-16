06c131eacdb612a867b054640c2dd5f4dbb8f85c
TheDaChicken <steve.rock.pet@gmail.com>
2019-06-15 19:00:00 -0700

    Changed message.

--- SERVER/PYTHON-FILES/Code/dataHandler.py
+++ SERVER/PYTHON-FILES/Code/dataHandler.py
@@ -50,7 +50,7 @@ def createDataFile():
 
 def loadData():
     if not os.path.isfile(data_yml_path):
-        warning("Unable to find data.yml! Should have been created unless being ran remotely.")
+        warning("Unable to find data.yml! Should have been created unless ran remotely.")
         return None
     with open(data_yml_path, 'r') as stream:
         yaml_config = yaml.load(stream, Loader=yaml.FullLoader)
