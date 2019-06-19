c7c975f46cde24083d8ae18075f180d861bf35d8
TheDaChicken <steve.rock.pet@gmail.com>
2019-06-19 15:57:18 -0700

    Remove return.

--- SERVER/PYTHON-FILES/Code/youtube/channelClass.py
+++ SERVER/PYTHON-FILES/Code/youtube/channelClass.py
@@ -148,7 +148,6 @@ class ChannelInfo:
             if "live_playback" not in yt_player_config:
                 self.video_id = None
                 self.privateStream = True
-                return [True, "OK"]
             else:
                 self.video_id = try_get(yt_player_config, lambda x: x['video_id'], str)
                 self.privateStream = False
