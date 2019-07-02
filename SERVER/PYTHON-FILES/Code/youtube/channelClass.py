4365af27ef5c19c9435897fe5423634fe0ab6c30
TheDaChicken <steve.rock.pet@gmail.com>
2019-07-02 15:25:14 -0700

    Added something... lol. [SERVER]

--- SERVER/PYTHON-FILES/Code/youtube/channelClass.py
+++ SERVER/PYTHON-FILES/Code/youtube/channelClass.py
@@ -296,6 +296,12 @@ class ChannelInfo:
                 if self.stop_heartbeat is False:
                     # LOOP
                     boolean_live = self.is_live(alreadyChecked=alreadyChecked)
+                    """
+                    boolean_live returns: 
+                        live: True
+                        non-live: False
+                        error: 1
+                    """
                     if boolean_live == 1:
                         # IF CRASHED.
                         info("Waiting 1 seconds then, trying heartbeat again.")
