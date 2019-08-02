d12d97fb25f4f525fc245ecd4c3fb7d85687a8d7
TheDaChicken <steve.rock.pet@gmail.com>
2019-08-02 15:15:25 -0700

    Cleaned up serverFunctions [CLIENT]

--- CLIENT/PYTHON-FILES/ServerFunctions.py
+++ CLIENT/PYTHON-FILES/ServerFunctions.py
@@ -4,13 +4,11 @@ from log import stopped
 DefaultHeaders = {'Client': 'PYTHON-CLIENT'}
 useHTTPS = False
 
-
-def format_response(html_reply):
-    if html_reply is None:
-        stopped("Lost Connection of the Server!")
-    if "OK" in html_reply['status']:
-        return [True, html_reply['response']]
-    return [False, html_reply['response']]
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    urlencode = None
+    stopped("Unsupported version of Python. You need Version 3 :<")
 
 
 def check_server(ip, port):
@@ -28,88 +26,96 @@ def check_server(ip, port):
     return True
 
 
+def __server_reply(ip, port, function_name, arguments, RequestMethod='GET'):
+    def format_response():
+        if dict_json is None:
+            stopped("Lost Connection of the Server!")
+        if "OK" in dict_json['status']:
+            return [True, dict_json['response']]
+        return [False, dict_json['response']]
+
+    dict_json = download_json('{0}{1}:{2}/{3}?{4}'.format((
+        "https://" if useHTTPS else 'http://'), ip, port, function_name, urlencode(arguments)), Headers=DefaultHeaders,
+        RequestMethod=RequestMethod)
+    return format_response()
+
+
 def add_channel(ip, port, channel_id):
-    html_reply = download_json(
-        ("https://" if useHTTPS else 'http://') + ip + ":" + port + "/addChannel?channel_id=" + channel_id,
-        Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'addChannel'
+    arguments = {'channel_id': channel_id}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def remove_channel(ip, port, channel_id):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port +
-                               "/removeChannel?channel_id=" + channel_id, Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'removeChannel'
+    arguments = {'channel_id': channel_id}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def get_channel_info(ip, port):
-    html_reply = download_json(("https://" if useHTTPS is True else 'http://') + ip + ":" + port + "/channelInfo",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'channelInfo'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def get_server_settings(ip, port):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/getServerSettings",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'getServerSettings'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def swap_settings(ip, port, setting_name):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/swap/" + setting_name,
-                               Headers=DefaultHeaders,
-                               RequestMethod='POST')
-    return format_response(html_reply)
+    function_name = 'swap/{0}'.format(setting_name)
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments, RequestMethod='POST')
 
 
 def get_youtube_api_info(ip, port):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/getYouTubeAPIInfo",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'getYouTubeAPIInfo'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def youtube_login(ip, port):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/getLoginURL",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'getLoginURL'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def youtube_logout(ip, port):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/logoutYouTubeAPI",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'logoutYouTubeAPI'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def test_upload(ip, port, channel_id):
-    html_reply = download_json(
-        ("https://" if useHTTPS else 'http://') + ip + ":" + port + "/testUpload?channel_id=" +
-        channel_id,
-        Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'testUpload'
+    arguments = {'channel_id': channel_id}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def youtube_fully_login(ip, port, username, password):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/youtubeLOGIN?username="
-                               + username +
-                               '&password=' + password,
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'youtubeLOGIN'
+    arguments = {'username': username, 'password': password}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def update_data_cache(ip, port):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/updateDataCache",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'updateDataCache'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def youtube_fully_logout(ip, port):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/youtubeLOGout",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'youtubeLOGout'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def listRecordings(ip, port):
-    html_reply = download_json(("https://" if useHTTPS else 'http://') + ip + ":" + port + "/listRecordings",
-                               Headers=DefaultHeaders)
-    return format_response(html_reply)
+    function_name = 'listRecordings'
+    arguments = {}
+    return __server_reply(ip, port, function_name, arguments)
 
 
 def playbackRecording(ip, port, RecordingName):
@@ -136,14 +142,3 @@ def downloadRecording(ip, port, RecordingName):
                     RecordingName, Headers=DefaultHeaders)
     FFmpeg.start_recording()
     return FFmpeg
-
-
-def playHLSStream(url):
-    from encoder import FFplay
-    try:
-        from urllib.parse import urlencode
-    except ImportError:
-        stopped("Unsupported version of Python. You need Version 3 :<")
-    FFplay = FFplay(url, Headers=DefaultHeaders)
-    FFplay.start_playback()
-    return FFplay
