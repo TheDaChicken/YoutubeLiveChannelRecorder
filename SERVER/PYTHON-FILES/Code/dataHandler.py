e542566cf80c28929765814de2d2f271bfd5d0e8
TheDaChicken <steve.rock.pet@gmail.com>
2019-12-09 15:48:16 -0800

    Recode-ish of dataHandler. >_>

--- SERVER/PYTHON-FILES/Code/dataHandler.py
+++ SERVER/PYTHON-FILES/Code/dataHandler.py
@@ -1,32 +1,73 @@
 import io
-import os
-from Code.log import info, warning
+from os import getcwd, mkdir
+from os.path import exists, join
+
 import yaml
 
-data_yml_path = os.path.join(os.getcwd(), "Data", "data.yml")
-data_yml_dir = os.path.join(os.getcwd(), "Data")
+from log import info
 
 
 class CacheDataHandler:
-    """
-
-    Used to cache stuff in data handler
-
-    """
+    yml_name = "data.yml"
     _cache = None
+    defaults = {
+        'channel_ids': [],
+        'DownloadThumbnail': True,
+        'recordingResolution': 'original',
+        'UploadLiveStreams': False,
+        'UploadThumbnail': False,
+        'UploadSettings': {
+            None: {
+                'CategoryID': 20,
+                'description': [
+                    'This is a automated video uploaded with "channelarchiver".',
+                    'Author of Program: TheDaChicken.',
+                    'Original Title: {TITLE}',
+                    'Original Video Link: https://youtu.be/{VIDEO_ID}',
+                    'The Original Video Link may be deleted.',
+                    'NOTE: When a streamer goes offline, and comes back online, 2 videos will uploaded.',
+                    '',
+                    '',
+                    'Original Description:',
+                    '',
+                    '{DESCRIPTION}'
+                ],
+                'privacyStatus': 'unlisted',
+                'tags': None,
+                'title': '{CHANNEL_NAME} {VIDEO_ID} {START_DATE_MONTH}/{START_DATE_DAY}/{START_DATE_YEAR}'
+            }
+        }
+    }
+
+    def loadDataFile(self):
+        data_folder = join(getcwd(), "Data")
+        if not exists(data_folder):
+            mkdir(data_folder)
+        if not exists(join(data_folder, self.yml_name)):
+            with io.open(join(data_folder, self.yml_name), 'w', encoding='utf8') as outfile:
+                outfile.write(yaml.dump(self.defaults))
+            info("Created Data.yml (Holds information like list of channels to check if live and record)")
+            self._cache = self.defaults
+        else:
+            with open(join(data_folder, self.yml_name), 'r', encoding='utf8') as stream:
+                self._cache = yaml.load(stream, Loader=yaml.FullLoader)
+
+    def saveData(self):
+        data_folder = join(getcwd(), "Data")
+        with io.open(join(data_folder, self.yml_name), 'w', encoding='utf8') as outfile:
+            outfile.write(yaml.dump(self._cache))
 
     def __init__(self):
-        setupDataFile()
-        self._cache = loadDataFile()
+        pass
 
     def updateCache(self):
-        self._cache = loadDataFile()
+        self.loadDataFile()
 
     def updateValue(self, key_name, value):
         self._cache.update({
             key_name: value,
         })
-        saveData(self._cache)
+        self.saveData()
 
     def getValue(self, key_name):
         """
@@ -41,77 +82,22 @@ class CacheDataHandler:
         if key_name not in self._cache:
             self._cache.update({key_name: []})
         self._cache.get(key_name).append(value)
-        saveData(self._cache)
+        self.saveData()
 
     def removeValueList(self, key_name, value):
         if value in self._cache.get(key_name):
             self._cache.get(key_name).remove(value)
-            saveData(self._cache)
+            self.saveData()
 
     def setValue(self, key_name, value):
         self._cache.update({
             key_name: value
         })
-        saveData(self._cache)
+        self.saveData()
 
     def deleteKey(self, key_name):
         del self._cache[key_name]
-        saveData(self._cache)
+        self.saveData()
 
     def getDict(self):
         return self._cache
-
-
-def setupDataFile():
-    # Default Yaml Settings
-    if not os.path.isfile(data_yml_path):
-        data = {
-            'channel_ids': [],
-            'DownloadThumbnail': True,
-            'recordingResolution': 'original',
-            'UploadLiveStreams': False,
-            'UploadThumbnail': False,
-            'UploadSettings': {
-                None: {
-                    'CategoryID': 20,
-                    'description': [
-                        'This is a automated video uploaded with "channelarchiver".',
-                        'Author of Program: TheDaChicken.',
-                        'Original Title: {TITLE}',
-                        'Original Video Link: https://youtu.be/{VIDEO_ID}',
-                        'The Original Video Link may be deleted.',
-                        'NOTE: When a streamer goes offline, and comes back online, 2 videos will uploaded.',
-                        '',
-                        '',
-                        'Original Description:',
-                        '',
-                        '{DESCRIPTION}'
-                    ],
-                    'privacyStatus': 'unlisted',
-                    'tags': None,
-                    'title': '{CHANNEL_NAME} {VIDEO_ID} {START_DATE_MONTH}/{START_DATE_DAY}/{START_DATE_YEAR}'
-                }
-            },
-        }
-        if os.path.exists(data_yml_dir) is False:
-            os.mkdir(data_yml_dir)
-        if os.path.isfile(data_yml_path) is False:
-            with io.open(data_yml_path, 'w', encoding='utf8') as outfile:
-                outfile.write(yaml.dump(data))
-            info("Created Data.yml (Holds information like list of channels to check if live and record)")
-
-
-def loadDataFile():
-    if not os.path.isfile(data_yml_path):
-        warning(
-            "Unable to find data.yml! Should have been created unless ran remotely.")
-        return None
-    with open(data_yml_path, 'r', encoding='utf8') as stream:
-        yaml_config = yaml.load(stream, Loader=yaml.FullLoader)
-    return yaml_config
-
-
-def saveData(data):
-    with io.open(data_yml_path, 'w', encoding='utf8') as outfile:
-        outfile.write(yaml.dump(data))
-
