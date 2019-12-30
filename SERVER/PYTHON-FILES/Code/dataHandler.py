73fa768c61e295f785e583b0a8f8e67c5cbfb6d6
TheDaChicken <steve.rock.pet@gmail.com>
2019-12-29 18:44:25 -0800

    Remove old stuff from the default data.yml. It now merges recordings. WHAT IS IT SAYING

--- SERVER/PYTHON-FILES/Code/dataHandler.py
+++ SERVER/PYTHON-FILES/Code/dataHandler.py
@@ -24,8 +24,6 @@ class CacheDataHandler:
                     'Author of Program: TheDaChicken.',
                     'Original Title: {TITLE}',
                     'Original Video Link: https://youtu.be/{VIDEO_ID}',
-                    'The Original Video Link may be deleted.',
-                    'NOTE: When a streamer goes offline, and comes back online, 2 videos will uploaded.',
                     '',
                     '',
                     'Original Description:',
