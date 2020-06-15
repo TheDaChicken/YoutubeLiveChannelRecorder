c0c13cb6405c87beb32058e8b4bf16152a987706
TheDaChicken <steve.rock.pet@gmail.com>
2020-06-14 21:27:20 -0700

    delete everything and start over

--- CLIENT/PYTHON-FILES/__main__.py
+++
@@ -1,7 +0,0 @@
-from ClientCode.screenHandler import ScreenHandler
-from ClientCode.screens import Sub_MainMenu
-
-if __name__ == '__main__':
-    screenHandler = ScreenHandler()
-    screenHandler.setScreen(Sub_MainMenu())
-    screenHandler.startScreenSystem()
